3119eb7af9d259232db34d536bec943074af2dba
Kamil75902 <105596542+Kamil75902@users.noreply.github.com>
2023-05-21 12:08:42 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,1 +1,48 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
 
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = var.app_service_plan_kind
+
+  sku {
+    tier = var.app_service_plan_tier
+    size = var.app_service_plan_size
+  }
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                      = var.function_app_name
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  storage_account_name      = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  os_type    = "Linux"
+  runtime_os = "linux"
+}
+
+resource "azurerm_function_app_function" "example" {
+  name              = var.function_name
+  resource_group_name = azurerm_resource_group.example.name
+  function_app_name = azurerm_linux_function_app.example.name
+  storage_account_name      = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME" = "dotnet"
+  }
+}
